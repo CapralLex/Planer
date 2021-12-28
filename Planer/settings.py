36e2b5621223a75fe3bec348b717a1c1bc2eadf0
CapralLex <lexxxoff@yandex.ru>
2021-12-28 08:45:51 +0300

    'Diploy'

--- Planer/settings.py
+++ Planer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%0@rodlm(u&mytvv#qv-k2t^nqr&v8f1bws#swlo+-i@$xpovw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
